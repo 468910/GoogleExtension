681d387722f267eac7f4f562b2d73a63ee50c7e5
Vroth <keesbank666@gmail.com>
2015-05-07 22:25:07 +0200

    Play and Pause for 9 media elements

--- vc.js
+++ vc.js
@@ -1,3 +1,24 @@
-function popup() {
-    alert("Hello World")
+var numOfVideos = 9;
+
+function vidplay() {
+    var button = document.getElementById("play");
+    var vids = [];
+    
+    for(var i = 0; i < numOfVideos; i++){
+        vids.push(document.getElementById("vid" + i));
+        console.log(vids.length);
+    }
+
+ 
+    if(vids[0].paused){
+        for(var i = 0; i < numOfVideos; i++){
+            vids[i].play();
+        }
+        button.textContent = "||"; 
+    }else{
+        for(var i = 0; i < numOfVideos; i++){
+            vids[i].pause();
+        }
+        button.textContent = ">";
+   }
 }
